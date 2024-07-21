3806c34cd37544a5b58f2e1fd5ef8f2605129c10
muallamubeen <107762644+muallamubeen@users.noreply.github.com>
2024-07-21 17:11:30 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("navLinks");
+    if (x.style.display === "inline-block") {
+    x.style.display = "none";
+    } else {
+    x.style.display = "inline-block";
+    }
+}
